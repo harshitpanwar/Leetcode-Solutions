b944c18abc223ecba6afdbe2ba32840fa293b71b
Harshit Panwar <54681032+harshitpanwar@users.noreply.github.com>
2023-01-12 18:40:39 +0530

    Time: 1061 ms (49.63%), Space: 377.5 MB (6.67%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:     
     
-   void dfs(int i, string &l, vector<vector<int>>& al, int cnt[], vector<int> &res) {
-    if (res[i] == 0) {
-        res[i] = 1;
-        for (auto j : al[i]) {
-            int cnt1[26] = {};
-            dfs(j, l, al, cnt1, res);
-            for (auto k = 0; k < 26; ++k)
-                cnt[k] += cnt1[k];
+    vector<int> mergemap(vector<int> &mp, vector<int> &m){
+        
+        vector<int> ans(26);
+        for(int i=0;i<26;i++){
+            ans[i]=mp[i]+m[i];
         }
-        res[i] = ++cnt[l[i] - 'a'];
+        
+        return ans;
     }
-}
-vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-    vector<int> res(n);
-    int cnt[26] = {};
-    vector<vector<int>> al(n);
-    for (auto &e : edges) {
-        al[e[0]].push_back(e[1]);
-        al[e[1]].push_back(e[0]);
+    
+    vector<int> dfs(unordered_map<int, vector<int>> &g, string &labels, int cur, int prev, vector<int> &ans){
+        
+        vector<int> mp(26);
+        
+        mp[labels[cur]-'a']=1;
+        vector<int> temp(26);
+        for(auto i:g[cur]){
+            
+            if(i!=prev){
+                temp = dfs(g, labels, i, cur, ans);
+                mp = mergemap(mp, temp);
+                
+            }
+            
+        }
+        
+        ans[cur] = mp[labels[cur]-'a'];
+        
+        return mp;
+        
+        
     }
-    dfs(0, labels, al, cnt, res);
-    return res;
-}
+    
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        
+        vector<int> ans(n);
+        unordered_map<int, vector<int>> graph;
+        
+        for(auto i:edges){
+            graph[i[0]].push_back(i[1]);
+            graph[i[1]].push_back(i[0]);
+        }
+        
+        dfs(graph, labels, 0, -1, ans);
+        
+        return ans;
+        
+    }
 };
