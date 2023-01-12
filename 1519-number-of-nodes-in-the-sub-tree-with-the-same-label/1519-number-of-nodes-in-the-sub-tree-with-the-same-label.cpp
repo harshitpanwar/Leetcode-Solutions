fcb2c02a13d39e5d21dc8f043527539e358b44fe
Harshit Panwar <54681032+harshitpanwar@users.noreply.github.com>
2023-01-12 17:52:36 +0530

    Time: 1082 ms (48.15%), Space: 377.4 MB (6.67%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:     
+    
+    vector<int> mergemap(vector<int> &mp, vector<int> &m){
+        
+        vector<int> ans(26);
+        for(int i=0;i<26;i++){
+            ans[i]=mp[i]+m[i];
+        }
+        
+        return ans;
+    }
+    
+    vector<int> dfs(unordered_map<int, vector<int>> &g, string &labels, int cur, int prev, vector<int> &ans){
+        
+        vector<int> mp(26);
+        
+        mp[labels[cur]-'a']=1;
+        
+        for(auto i:g[cur]){
+            
+            if(i!=prev){
+                
+                vector<int> temp(26);
+                temp = dfs(g, labels, i, cur, ans);
+                mp = mergemap(mp, temp);
+                
+            }
+            
+        }
+        
+        ans[cur] = mp[labels[cur]-'a'];
+        
+        return mp;
+        
+        
+    }
+    
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        
+        vector<int> ans(n);
+        unordered_map<int, vector<int>> graph;
+        
+        for(auto i:edges){
+            graph[i[0]].push_back(i[1]);
+            graph[i[1]].push_back(i[0]);
+        }
+        
+        dfs(graph, labels, 0, -1, ans);
+        
+        return ans;
+        
+    }
+};
