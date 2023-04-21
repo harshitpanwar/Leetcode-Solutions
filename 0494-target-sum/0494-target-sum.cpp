1ec22481bb7a3ebf5ff3befeaab73f080f8a876a
Harshit Panwar <54681032+harshitpanwar@users.noreply.github.com>
2023-04-21 14:35:47 +0530

    Time: 31 ms (51.69%), Space: 39.9 MB (17.54%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     
-    int f(vector<int> &nums, int i, int sum, int target){
+    int f(vector<int> &nums, int i, int sum, int target, vector<vector<int>> &dp){
         
-        if(i==nums.size() and sum==target) return 1;
+        if(i==nums.size() and sum==target) return dp[i][sum+1000]=1;
         
+        if(dp[i][sum+1000]!=-1) return dp[i][sum+1000];
         
-        if(i>=nums.size()) return 0;
+        if(i>=nums.size()) return dp[i][sum+1000]=0;
         
-        int pick = f(nums, i+1, sum+nums[i], target);
-        int nopick = f(nums, i+1, sum-nums[i], target);
+        int pick = f(nums, i+1, sum+nums[i], target, dp);
+        int nopick = f(nums, i+1, sum-nums[i], target, dp);
         
-        return pick + nopick;   
+        return dp[i][sum+1000]=pick + nopick;   
         
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(nums, 0, 0, target);
+        vector<vector<int>> dp(nums.size()+1, vector<int>(2001,-1));
+        return f(nums, 0, 0, target, dp);
     }
 };
