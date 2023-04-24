cb5164c10c1b866d3c11b60c71ae4175d3555c3d
Harshit Panwar <54681032+harshitpanwar@users.noreply.github.com>
2023-04-24 09:57:03 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (41.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        priority_queue<int> maxh;
+        
+        for(auto i:stones) maxh.push(i);
+        
+        while(!maxh.empty()){
+            
+            int a=0, b=0;
+            if(!maxh.empty()) a = maxh.top();maxh.pop();
+            if(!maxh.empty()) b = maxh.top();
+            else return a;
+            maxh.pop();
+            
+            maxh.push(max(a-b, b-a));
+                
+            
+        }
+        
+        return maxh.top();
+        
+        
+    }
+};
