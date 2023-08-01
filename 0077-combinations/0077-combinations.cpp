600b8facb897b951a8b9cb961828cf01f4f9b989
Harshit Panwar <54681032+harshitpanwar@users.noreply.github.com>
2023-08-01 16:05:19 +0530

    Time: 11 ms (92.70%), Space: 19.1 MB (22.26%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -5,10 +5,14 @@ public:
     
     void f(int n, int k, int i, vector<int> &v){
         
+        
         if(i>n){
             if(v.size()==k) ans.push_back(v);
             return;
         }
+        
+        if(n-i+1 + v.size()<k) return;
+
         
         v.push_back(i);
         f(n, k, i+1, v);
