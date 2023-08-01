3424ff83d217133236d894196d04f4886badd348
Harshit Panwar <54681032+harshitpanwar@users.noreply.github.com>
2023-08-01 13:08:49 +0530

    Time: 18 ms (87.68%), Space: 19 MB (23.26%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    
+    void f(int n, int k, int i, vector<int> &v){
+        
+        if(i>n){
+            if(v.size()==k) ans.push_back(v);
+            return;
+        }
+        
+        v.push_back(i);
+        f(n, k, i+1, v);
+        v.pop_back();
+        
+        f(n, k, i+1, v);
+        
+        
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> v;
+        f(n, k, 1, v);
+        return ans;
+    }
+};
