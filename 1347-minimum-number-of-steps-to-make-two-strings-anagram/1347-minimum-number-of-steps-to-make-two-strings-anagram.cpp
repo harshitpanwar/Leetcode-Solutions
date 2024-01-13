a67f684ec4fd1ba4174f113c4e1104e63f93fe8e
Harshit Panwar <54681032+harshitpanwar@users.noreply.github.com>
2024-01-13 23:45:44 +0530

    Time: 36 ms (98.08%), Space: 16.8 MB (97.67%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         
-        unordered_map<char, int> mp1, mp2;
-        for(char i:s) mp1[i]++;
-        for(char i:t) mp2[i]++;
-        
+        int arr1[26] = {0};
         int ans = 0;
-        for(auto i: mp1){
-            
-            int freq1 = i.second;
-            int freq2 = mp2[i.first];
-            
-            ans += freq1>freq2?freq1-freq2:0;
-            
+        
+        for(int i=0;i<s.size();i++){
+            arr1[s[i]-'a']++;
+        }
+        
+        for(int i=0;i<t.size();i++){
+            arr1[t[i] - 'a']--;
+        }
+        
+        for(int i=0;i<26;i++){
+            ans += arr1[i]>0?arr1[i]:0; 
         }
         
         return ans;
